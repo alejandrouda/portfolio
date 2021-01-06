ba7018f363c85b5d64bd1706962fd357b2b8fb85
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2021-01-06 16:40:37 +0100

    aligning

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35839" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34269" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
