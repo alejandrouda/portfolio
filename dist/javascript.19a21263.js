4a568f763dfa83ea5562e81b2c3d08c52a6d2f6e
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2021-01-07 11:51:25 +0100

    styling form

--- dist/javascript.19a21263.js
+++ dist/javascript.19a21263.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37753" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42457" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
